9afaa4d6190d5ab7ee6a44d0ef9a986e88ac74fd
Iohann Rabeson <IohannRabeson@users.noreply.github.com>
2026-03-14 23:37:51 -0400

    tmignore 0.1.1

--- Formula/tmignore-rs.rb
+++ Formula/tmignore-rs.rb
@@ -3,8 +3,8 @@ class TmignoreRs < Formula
   homepage "https://github.com/IohannRabeson/tmignore-rs"
   url "https://github.com/IohannRabeson/tmignore-rs.git",
       using:    :git,
-      revision: "57749328dc0ecf0cc927896234077e945220f891"
-  version "0.1.0"
+      revision: "d04006d"
+  version "0.1.1"
   license "MIT"
   depends_on "rust" => :build
   depends_on :macos
